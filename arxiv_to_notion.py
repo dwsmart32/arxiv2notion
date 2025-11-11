b42ebc3a5458ce1a4682a1ef91d8ba1260bc9e9a
Dongwook Lee <70640776+dwsmart32@users.noreply.github.com>
2025-11-11 15:59:30 +0900

    Update arxiv_to_notion.py

--- arxiv_to_notion.py
+++ arxiv_to_notion.py
@@ -8,7 +8,8 @@ import time
 from google.genai import types
 import httpx
 import re
-
+import PyPDF2
+import io
 NOTION_TOKEN = os.environ.get("NOTION_TOKEN")
 DATABASE_ID = os.environ.get("DATABASE_ID")
 GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
